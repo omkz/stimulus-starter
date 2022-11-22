35c7918d4d0b4149457966a4c477ff506b68604a
Kurnia Muhamad <omenkzz@gmail.com>
2022-11-23 03:53:24 +0700

    Refactor greet method

--- src/controllers/hello_controller.js
+++ src/controllers/hello_controller.js
@@ -4,10 +4,10 @@ export default class extends Controller {
     static targets = ["name"]
 
     greet() {
-        // console.log("Hello, Stimulus!", this.element)
-        const element = this.nameTarget
-        const name = element.value
-        console.log(`Hello, ${name}`)
+        console.log(`Hello, ${this.name}`)
+    }
 
+    get name() {
+        return this.nameTarget.value
     }
 }
